3e1f116f93cbeca33b49df79884f081667901d1b
RennC <154007469+RennC@users.noreply.github.com>
2025-09-05 23:38:35 -0300

    Create user.py

---
+++ cognitapath_backend/app/schemas/user.py
@@ -0,0 +1,14 @@
+import uuid
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserInDB(UserBase):
+    id: uuid.UUID
+
+    class Config:
+        orm_mode = True
